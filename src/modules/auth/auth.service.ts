d3314eead1035da21f34ad67f4e5fe4e8b42b97a
Yash J Mac <yashjangid8078@gmail.com>
2026-05-30 17:37:32 +0530

    chore: reduce bcrypt rounds to 10 and remove unused guard imports from main.ts

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -11,7 +11,7 @@ import { LoginDto } from './dto/login.dto';
 import { SignupDto } from './dto/signup.dto';
 import { UserRole } from '../../common/constants/roles.constants';
 
-const BCRYPT_ROUNDS = 12;
+const BCRYPT_ROUNDS = 10;
 
 @Injectable()
 export class AuthService {
